2e0c0fd2647a1e13d2dd09fc2ed193893f0b569c
tktruc211 <tktruc19@clc.fitus.edu.vn>
2022-05-11 18:46:52 +0700

    update insert-data

--- Chính sách/insert-data.sql
+++ Chính sách/insert-data.sql
@@ -1,69 +1,99 @@
-insert into CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS001', 'Phòng Y tế Quận Bình Tân ', '521 Kinh Dương Vương Phường An Lạc Quận Bình Tân', '22451045');
-insert into CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS002', 'B?nh viện Nhi Đồng Thành phố', ' Xã Tân Kiên Huyện Bình Chánh', '22536688');
-insert into CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS003', 'Phòng Y tế Quận 8', 'số 4 Phạm Thế Hiển Phường 5 Quận 8', '34314341');
-insert into CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS004', 'B?nh viện Quận Bình Thạnh', '112AB Đinh Tiên Hoàng Phường 1 Quận Bình Thạnh', '35108966');
-insert into CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS005', 'Trạm Y tế Phường 15', '22/1 ?i?n Biên Phủ Phường 15 Quận Bình Thạnh', '35122143');
-select * from csyt;
---test
-select * from dba_users;
-select * from nhanvien;
-exec INSERTNHANVIEN ('NV001', N'Hồ Quang Danh ', 'Nam', '1970-07-07', '106143818423', 'Yên Bái ', '654113305', 'CS001', N'Thanh tra', NULL,'1234');
-exec INSERTNHANVIEN ('NV002', N'Biên Bình Ðạt ', 'Nữ', NULL, '518255758747', 'Quảng Ninh ', '897378822', 'CS002', N'Thanh tra', NULL,'1234');
-exec INSERTNHANVIEN ('NV003', N'Phong Thái Dương ', 'Nam', NULL, '799192604508', 'Tuyên Quang ', '295910497', 'CS003', N'Thanh tra', NULL, '1234');
-exec INSERTNHANVIEN ('NV004', N'Quán Anh Khôi ', 'Nam', NULL, '496355483978', 'Kiên Giang ', '713057827', 'CS004', N'Thanh tra', NULL,'1234');
-exec INSERTNHANVIEN ('NV005', N'Tông Phi Hai ', 'Nam', NULL, '122646930083', 'Vĩnh Long ', '207161212', 'CS005', N'Thanh tra', NULL,'1234');
-exec INSERTNHANVIEN ('NV006', N'Thành Tân Phu ', 'Nam', '1970-07-07', '106143818423', 'Yên Bái ', '654113305', 'CS001', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV007',N'Ninh Vinh Quoc ', 'N?', NULL, '518255758747', 'Quảng Ninh ', '897378822', 'CS002', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV008', 'Bo Ðac Tu ', 'Nam', NULL, '799192604508', 'Tuyên Quang ', '295910497', 'CS003', N'Cơ sở y tế', NULL, '1234');
-exec INSERTNHANVIEN ('NV009', 'Lộc Minh Thiên ', 'Nam', NULL, '496355483978', 'Kiên Giang ', '713057827', 'CS004', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV010', 'Lai Xuân Thien ', 'Nam', NULL, '122646930083', 'Vĩnh Long ', '207161212', 'CS005', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV011', 'Chính Thuận ', 'Nam', '1970-07-07', '106143818423', 'Yên Bái ', '654113305', 'CS001', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV012', 'Đinh Vang Việt ', 'N?', NULL, '518255758747', 'Quảng Ninh ', '897378822', 'CS002', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV013', 'Đoàn Thiên Ân ', 'Nam', NULL, '799192604508', 'Tuyên Quang ', '295910497', 'CS003', N'Cơ sở y tế', NULL, '1234');
-exec INSERTNHANVIEN ('NV014', 'Diêm Vân Anh ', 'Nam', NULL, '496355483978', 'Kiên Giang ', '713057827', 'CS004', N'Cơ sở y tế', NULL,'1234');
-exec INSERTNHANVIEN ('NV015', 'Giàng Tân Bình ', 'Nam', NULL, '122646930083', 'Vĩnh Long ', '207161212', 'CS005', N'Cơ sở y tế', NULL,'1234');
+-- INSERT DATA CHUYENKHOA
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0001', N'Sản phụ khoa và hỗ trợ sinh sản');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0002', N'Tim mạch');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0003', N'Nhi');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0004', N'Ung bướu - Xạ trị');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0005', N'Sức khỏe tổng quát');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0006', N'Tiêu hóa - Gan mật');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0007', N'Cơ xương khớp');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0008', N'Tế bào gốc và Công nghệ gen');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0009', N'Ngân hàng Mô');
+INSERT INTO CHUYENKHOA(MAKHOA,TENKHOA) VALUES ('CK0010', N'Y Học Cổ Truyền');
 
-exec INSERTNHANVIEN ('NV016', 'Huệ Hương ', 'N?', NULL, '518255758747', 'Qu?ng Ninh ', '897378822', 'CS002', N'Y sĩ / Bác sĩ', 'Tai, mũi, họng','1234');
-exec INSERTNHANVIEN ('NV017', 'Ninh Vinh Quốc ', 'N?', NULL, '518255758747', 'Qu?ng Ninh ', '897378822', 'CS002', N'Y sĩ / Bác sĩ', 'Tai, mũi, họng','1234');
-exec INSERTNHANVIEN ('NV018', 'Tán Khánh Mai ', 'Nam', NULL, '799192604508', 'Tuyên Quang ', '295910497', 'CS003', 'Y sĩ / Bác sĩ','Tai, mũi, họng', '1234');
-exec INSERTNHANVIEN ('NV019', 'Lôc Minh Thiên ', 'Nam', NULL, '496355483978', 'Kiên Giang ', '713057827', 'CS004', 'Y sĩ / Bác sĩ', 'Tai, m?i, h?ng','1234');
-exec INSERTNHANVIEN ('NV020', 'Lộc Minh ', 'Nam', NULL, '496355483543', 'Kiên Giang ', '71304563', 'CS004', 'Y sĩ / Bác sĩ', 'Tr? em','1234');
-exec INSERTNHANVIEN ('NV021', 'Hàn Kim Thủy ', 'Nam', NULL, '496355483543', 'Kiên Giang ', '71304563', 'CS004', 'Y sĩ / Bác sĩ', 'Th?n kinh','1234');
-exec INSERTNHANVIEN ('NV022', 'Lâm Oanh ', 'N?', NULL, '518255758747', 'Qu?ng Ninh ', '897378822', 'CS002', 'Y sĩ / Bác sĩ', 'Tr? em','1234');
-exec INSERTNHANVIEN ('NV023', 'Chung Lệ Chi ', 'Nam', NULL, '799192604508', 'Tuyên Quang ', '295910497', 'CS003', 'Y sĩ / Bác sĩ', 'Th?n kinh', '1234');
-exec INSERTNHANVIEN ('NV024', 'Phù Linh Châu ', 'Nam', NULL, '496355483978', 'Kiên Giang ', '713057827', 'CS004', 'Y sĩ / Bác sĩ', 'Ung th?','1234');
-exec INSERTNHANVIEN ('NV025', 'Hùng Kièu Nương ', 'Nam', NULL, '122646930083', 'Vĩnh Long ', '207161212', 'CS005', 'Y sĩ / Bác sĩ', 'Ung th?','1234');
+-- INSERT DATA DICHVU
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0001', N'Thai sản trọn gói');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0002', N'Gói sàng lọc giáp');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0003', N'Máu cuống rốn');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0004', N'Khám sức khỏe tổng quát');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0005', N'Sàng lọc ung thư');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0006', N'Bảo hiểm & Chương trình hội viên');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0007', N'Sàng lọc gan mật');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0008', N'Nha khoa');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0009', N'Sàng lọc tim mạch');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0010', N'Các gói dịch vụ khác');
+INSERT INTO DICHVU(MADV,TENDICHVU) VALUES ('DV0011', N'Ưu đãi');
 
-exec INSERTNHANVIEN ('NV026', 'Chung Quang Th?nh ', 'N?',null, '716299399663', '??k Nông ', '416443922', 'CS001', 'Nghiên cứu', 'Vô tuy?n h?c can thi?p','1234');
-exec INSERTNHANVIEN ('NV027', 'Giao Anh Vi?t ', 'Nam', NULL, '728016968058', 'Trà Vinh ', '173338081',  'CS003', 'Nghiên cứu', 'Nhi th?n', '1234');
-exec INSERTNHANVIEN ('NV028', 'Trà Qu?c Vi?t ', 'N?',null, '818705368116', 'H?u Giang ', '176715229', 'CS004', 'Nghiên cứu', 'B?nh lý h?c','1234');
-exec INSERTNHANVIEN ('NV029', 'L?c M?nh ', 'Nam', NULL, '496355483543', 'Kiên Giang ', '71304563', 'CS001', 'Nghiên cứu', 'B?nh lý h?c','1234');
-exec INSERTNHANVIEN ('NV030', 'Hàn Kim Th?y ', 'Nam', NULL, '496355483543', 'Kiên Giang ', '71304563', 'CS005', 'Nghiên cứu', 'B?nh lý h?c','1234');
-exec INSERTNHANVIEN ('NV031', '??i Lâm Oanh ', 'N?', NULL, '518255758747', 'Qu?ng Ninh ', '897378822', 'CS002', 'Nghiên cứu', 'B?nh lý h?c','1234');
+-- INSERT DATA CSYT
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0001', 'Bệnh viện Đa khoa Quốc tế Central Park', '208 Nguyễn Hữu Cảnh, Phường 22, Q.Bình Thạnh, Hồ Chí Minh', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0002', 'Phòng khám Đa khoa Quốc tế Sài Gòn', '2 - 2 Bis Trần Cao Vân, P. Đa Kao, Q. 1, TP. Hồ Chí Minh', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0003', 'Bệnh viện Đa khoa Quốc tế Times City', '458 Minh Khai, Q. Hai Bà Trưng, Hà Nội', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0004', 'Phòng khám Quốc tế Royal City', 'Tầng 1, tòa R2, Khu Đô thị Royal City 72A Nguyễn Trãi, Q. Thanh Xuân, Hà Nội', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0005', 'Phòng khám Đa khoa Quốc tế Times City', 'Số 458 Minh Khai, P. Vĩnh Tuy, Q. Hai Bà Trưng, Hà Nội', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0006', 'Bệnh viện Đa khoa Quốc tế Đà Nẵng', 'Đường 30 Tháng 4, Khu dân cư số 4 Nguyễn Tri Phương, P. Hòa Cường Bắc, Q. Hải Châu, TP Đà Nẵng', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0007', 'Bệnh viện Đa khoa Quốc tế Nha Trang', '42A Trần Phú, Vĩnh Nguyên, Nha Trang', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0008', 'Bệnh viện Đa khoa Quốc tế Hải Phòng', 'Võ Nguyên Giáp, Q. Lê Chân, TP. Hải Phòng', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0009', 'Bệnh viện Đa khoa Quốc tế Hạ Long', '10A Lê Thánh Tông, Hồng Gai, TP. Hạ Long', '1900232389');
+INSERT INTO CSYT (MACSYT, TENCSYT, DCCSYT, SDTCSYT) values ('CS0010', 'Bệnh viện Đa khoa Quốc tế Phú Quốc', 'Khu Bãi Dài, X. Gành Dầu, Phú Quốc', '1900232389');
+--select * from csyt;
 
-select * from benhnhan;
-exec deletebenhnhan('BN002');
--- INSERT DATA INTO BENHNHAN
-EXEC INSERTBENHNHAN('BN001','CS002', N'H?ng B?ch Th?o','592218019912', NULL, '12', 'Ph??ng Canh', 'Qu?n Nam T? Liêm', 'Hà N?i', NULL, NULL,null,'1234');
-EXEC INSERTBENHNHAN('BN002','CS003', N'Quang Thái','3655745235324', '1945-02-03', '421', 'Ph??ng Canh', 'Qu?n Nam T? Liêm', 'Hà N?i', NULL, NULL, NULL,'1234B');
-EXEC INSERTBENHNHAN('BN003','CS004', N'Kh?c Vi?t','536437346436', '1955-06-30', '101','???ng H?i H?', 'Qu?n H?i Châu', '?à N?ng', NULL, 'R?i lo?n ch?y máu', NULL,'1234');
-EXEC INSERTBENHNHAN('BN004','CS005', N'Ti?n Ð?c','634257654325', '1960-05-25', '235', '???ng Lê Thanh Ngh?', 'Qu?n H?i Châu', '?à N?ng', NULL, NULL, NULL,'1234');
-EXEC INSERTBENHNHAN('BN005','CS001', N'H? Th? Lan','563454765352', '1975-09-06', '352','???ng ?i?n Biên Ph?', 'Qu?n Thanh Khê', '?à N?ng', NULL, NULL, NULL,'1234');
-EXEC INSERTBENHNHAN('BN006','CS001', N'Ch?n Hùng', '966396551955','1999-11-01', '133', '???ng Tr?n Cao Vân', 'Qu?n Thanh Khê', '?à N?ng', 'R?i lo?n ch?y máu', NULL, NULL,'1234');
-EXEC INSERTBENHNHAN('BN007','CS001', N'Lê Th? H?ng','09314225w334', '1955-05-22', '365', N'Bà HUy?n Thanh Quan', N'Q.Bình Th?nh', N'TPHCM', N'U x? t? cung', N'B?nh tim', N'Paracetamol','1234');
+-- INSERT DATA NHANVIEN
+exec INSERTNHANVIEN ('NV0001', N'Hồ Quang Danh', N'Nam', '1970-07-07', '106143818423', N'Yên Bái', '654113305', 'CS0001', N'Thanh tra', 'CK0001', '1234');
+exec INSERTNHANVIEN ('NV0002', N'Biên Bình Ðạt', N'Nữ', NULL, '518255758747', N'Quảng Ninh', '897378822', 'CS0002', N'Thanh tra', 'CK0005', '1234');
+exec INSERTNHANVIEN ('NV0003', N'Phong Thái Dương', N'Nam', NULL, '799192604508', N'Tuyên Quang', '295910497', 'CS0003', N'Thanh tra', 'CK0010', '1234');
+exec INSERTNHANVIEN ('NV0004', N'Quán Anh Khôi', N'Nam', NULL, '496355483978', N'Kiên Giang', '713057827', 'CS0004', N'Thanh tra', 'CK0002','1234');
+exec INSERTNHANVIEN ('NV0005', N'Tông Phi Hai', N'Nam', NULL, '122646930083', N'Vĩnh Long', '207161212', 'CS0005', N'Thanh tra', 'CK0003', '1234');
 
-INSERT INTO HSBA(MAHSBA,MABN,NGAY,CHANDOAN,MABS,MAKHOA,MACSYT,KETLUAN) 
-VALUES ('HSBA001', 'BN007', TO_DATE('2022-03-12', 'yyyy-mm-dd'), 'Ti?u ???ng', 'NV026', 'K001', 'CS001', 'Ti?u ???ng mãn tính');
-INSERT INTO HSBA(MAHSBA,MABN,NGAY,CHANDOAN,MABS,MAKHOA,MACSYT,KETLUAN) 
-VALUES ('HSBA002', 'BN002', TO_DATE('2022-05-05', 'yyyy-mm-dd'), 'Suy hô h?p  ', 'NV027', 'K002', 'CS002', 'Viêm ph?i c?ng ??ng');
-INSERT INTO HSBA(MAHSBA,MABN,NGAY,CHANDOAN,MABS,MAKHOA,MACSYT,KETLUAN) 
-VALUES ('HSBA003', 'BN003', TO_DATE('2022-05-08', 'yyyy-mm-dd'), '?au d? dày', 'NV028', 'K003', 'CS003', 'Viêm loát d? dày');
-insert into HSBA (MAHSBA, MABN, NGAY, CHANDOAN, MABS, MAKHOA, MACSYT, KETLUAN) values ('HSBA004', 'BN001', TO_DATE('2020-12-01', 'YYYY-MM-DD'),'Sinh thi?t th?n ghép sau ghép th?n d??i h??ng d?n c?a siêu âm','NV016', 'K001', 'CS001','Nhi?m trùng do t? c?u vàng');
+exec INSERTNHANVIEN ('NV0006', N'Thành Tân Phu', N'Nam', '1970-07-07', '106143818423', N'Yên Bái', '654113305', 'CS0001', N'Cơ sở y tế', 'CK0001', '1234');
+exec INSERTNHANVIEN ('NV0007', N'Ninh Vinh Quốc', N'Nữ', NULL, '518255758747', N'Quảng Ninh', '897378822', 'CS0002', N'Cơ sở y tế', 'CK0005', '1234');
+exec INSERTNHANVIEN ('NV0008', N'Bo Ðac Tu', N'Nam', NULL, '799192604508', N'Tuyên Quang', '295910497', 'CS0003', N'Cơ sở y tế', 'CK0009', '1234');
+exec INSERTNHANVIEN ('NV0009', N'Lộc Minh Thiên', N'Nam', NULL, '496355483978', N'Kiên Giang', '713057827', 'CS0004', N'Cơ sở y tế', 'CK0008', '1234');
+exec INSERTNHANVIEN ('NV0010', N'Lai Xuân Thiên', N'Nam', NULL, '122646930083', N'Vĩnh Long', '207161212', 'CS0005', N'Cơ sở y tế', 'CK0006', '1234');
+exec INSERTNHANVIEN ('NV0011', N'Chính Thuận', N'Nam', '1970-07-07', '106143818423', N'Yên Bái', '654113305', 'CS0001', N'Cơ sở y tế', 'CK0003', '1234');
+exec INSERTNHANVIEN ('NV0012', N'Đinh Vang Việt', N'Nữ', NULL, '518255758747', N'Quảng Ninh', '897378822', 'CS0002', N'Cơ sở y tế', 'CK0010', '1234');
+exec INSERTNHANVIEN ('NV0013', N'Đoàn Thiên Ân', N'Nam', NULL, '799192604508', N'Tuyên Quang', '295910497', 'CS0003', N'Cơ sở y tế', 'CK0004', '1234');
+exec INSERTNHANVIEN ('NV0014', N'Diêm Vân Anh', N'Nam', NULL, '496355483978', N'Kiên Giang', '713057827', 'CS0004', N'Cơ sở y tế', 'CK0007', '1234');
+exec INSERTNHANVIEN ('NV0015', N'Giàng Tân Bình', N'Nam', NULL, '122646930083', N'Vĩnh Long', '207161212', 'CS0005', N'Cơ sở y tế', 'CK0002', '1234');
+    
+exec INSERTNHANVIEN ('NV0016', N'Huệ Hương', N'Nữ', NULL, '518255758747', N'Quảng Ninh', '897378822', 'CS0002', N'Y sĩ/Bác sĩ', 'CK0001','1234');
+exec INSERTNHANVIEN ('NV0017', N'Ninh Vinh Quốc', N'Nữ', NULL, '518255758747', N'Quảng Ninh', '897378822', 'CS0002', N'Y sĩ/Bác sĩ', 'CK0002','1234');
+exec INSERTNHANVIEN ('NV0018', N'Tán Khánh Mai', N'Nam', NULL, '799192604508', N'Tuyên Quang', '295910497', 'CS0003', N'Y sĩ/Bác sĩ', 'CK0003', '1234');
+exec INSERTNHANVIEN ('NV0019', N'Lôc Minh Thiên', N'Nam', NULL, '496355483978', N'Kiên Giang', '713057827', 'CS0004', N'Y sĩ/Bác sĩ', 'CK0004','1234');
+exec INSERTNHANVIEN ('NV0020', N'Lộc Minh', N'Nam', NULL, '496355483543', N'Kiên Giang', '71304563', 'CS0004', N'Y sĩ/Bác sĩ', 'CK0005','1234');
+exec INSERTNHANVIEN ('NV0021', N'Hàn Kim Thủy', N'Nam', NULL, '496355483543', 'Kiên Giang', '71304563', 'CS0004', N'Y sĩ/Bác sĩ', 'CK0006','1234');
+exec INSERTNHANVIEN ('NV0022', N'Lâm Oanh', N'Nữ', NULL, '518255758747', N'Quảng Ninh', '897378822', 'CS0002', N'Y sĩ/Bác sĩ', 'CK0007', '1234');
+exec INSERTNHANVIEN ('NV0023', N'Chung Lệ Chi', N'Nam', NULL, '799192604508', N'Tuyên Quang', '295910497', 'CS0003', N'Y sĩ/Bác sĩ', 'CK0008', '1234');
+exec INSERTNHANVIEN ('NV0024', N'Phù Linh Châu', N'Nam', NULL, '496355483978', N'Kiên Giang', '713057827', 'CS0004', N'Y sĩ/Bác sĩ', 'CK0009', '1234');
+exec INSERTNHANVIEN ('NV0025', N'Hùng Kièu Nương', N'Nam', NULL, '122646930083', N'Vĩnh Long', '207161212', 'CS0005', N'Y sĩ/Bác sĩ', 'CK0010','1234');
 
---INSERT DATA TO HSBA_DV
-INSERT INTO HSBA_DV(MAHSBA,MADV,NGAY,MAKTV,KETQUA)
-VALUES ('HSBA001', 'DV001',TO_DATE('2022-03-12', 'yyyy-mm-dd'), 'NV027', 'Ti?u ???ng');
-INSERT INTO HSBA_DV(MAHSBA,MADV,NGAY,MAKTV,KETQUA)
-VALUES ('HSBA002', 'DV002',TO_DATE('2022-05-12', 'yyyy-mm-dd'), 'NV025', 'Viêm ph?i');
-INSERT INTO HSBA_DV(MAHSBA,MADV,NGAY,MAKTV,KETQUA)
-VALUES ('HSBA003', 'DV003',TO_DATE('2022-05-12', 'yyyy-mm-dd'), 'NV020', 'Viêm d? dày');
+exec INSERTNHANVIEN ('NV0026', N'Chung Quang Thành', N'Nữ', NULL, '716299399663', N'Đắk Nông', '416443922', 'CS0001', N'Nghiên cứu', 'CK0005', '1234');
+exec INSERTNHANVIEN ('NV0027', N'Giao Anh Việt', N'Nam', NULL, '728016968058', N'Trà Vinh', '173338081',  'CS0003', N'Nghiên cứu', 'CK0003', '1234');
+exec INSERTNHANVIEN ('NV0028', N'Trà Quốc Việt', N'Nữ', NULL, '818705368116', N'Hậu Giang', '176715229', 'CS0004', N'Nghiên cứu', 'CK0006','1234');
+exec INSERTNHANVIEN ('NV0029', N'Lộc Minh', N'Nam', NULL, '496355483543', N'Kiên Giang', '71304563', 'CS0001', N'Nghiên cứu', 'CK0009','1234');
+exec INSERTNHANVIEN ('NV0030', N'Hàn Kim Thủy', N'Nam', NULL, '496355483543', N'Kiên Giang', '71304563', 'CS0005', N'Nghiên cứu', 'CK0010','1234');
+--select * from dba_users;
+--select * from nhanvien;
+
+-- INSERT DATA BENHNHAN
+EXEC INSERTBENHNHAN('BN0001','CS0002', N'Hằng Bích Thảo','592218019912', NULL, '12', N'Phươnng Canh', N'Quận Nam Từ Liêm', N'Hà Nội', NULL, NULL, NULL, '1234');
+EXEC INSERTBENHNHAN('BN0002','CS0003', N'Quang Thái','3655745235324', '1945-02-03', '421', N'Phươnng Canh', N'Quận Nam Từ Liêm', N'Hà Nội', NULL, NULL, NULL, '1234B');
+EXEC INSERTBENHNHAN('BN0003','CS0004', N'Khắc Việt','536437346436', '1955-06-30', '101', N'Thông Tây Hội', N'Quận Hai Châu', N'Đà Nẵng', NULL, NULL, NULL, '1234');
+EXEC INSERTBENHNHAN('BN0004','CS0005', N'Tiến Ðức','634257654325', '1960-05-25', '235', N'Lê Thanh Nghi', N'Quận Hai Châu', N'Đà Nẵng', NULL, NULL, NULL, '1234');
+EXEC INSERTBENHNHAN('BN0005','CS0001', N'Hà Thị Lan','563454765352', '1975-09-06', '352', N'Điện Biên Phủ', N'Quận Thanh Khê', N'Đà Nẵng', NULL, NULL, NULL, '1234');
+EXEC INSERTBENHNHAN('BN0006','CS0001', N'Chấn Hùng', '966396551955','1999-11-01', '133', N'Trần Cao Vân', N'Quận Thanh Khê', N'Đà Nẵng', NULL, NULL, NULL, '1234');
+EXEC INSERTBENHNHAN('BN0007','CS0001', N'Lê Thị Hằng','09314225w334', '1955-05-22', '365', N'Bà Huyện Thanh Quan', N'Q.Bình Thạnh', N'TP.HCM', NULL, NULL, NULL, '1234');
+--select * from benhnhan;
+--exec deletebenhnhan('BN002');
+
+--INSERT DATA HSBA
+INSERT INTO HSBA (MAHSBA, MABN, NGAY, CHANDOAN, MABS, MAKHOA, MACSYT, KETLUAN) values ('HS0001', 'BN0001', '2020-01-01', N'Ghi điện cơ cấp cứu', 'NV0016', 'CK0001', 'CS0001', N'Chấn thương dây chằng chéo trước');
+INSERT INTO HSBA (MAHSBA, MABN, NGAY, CHANDOAN, MABS, MAKHOA, MACSYT, KETLUAN) values ('HS0002', 'BN0003', '2020-01-02', N'Chụp cắt lớp vi tính động mạch vành', 'NV0017', 'CK0002', 'CS0002', N'Sa trực tràng');
+INSERT INTO HSBA (MAHSBA, MABN, NGAY, CHANDOAN, MABS, MAKHOA, MACSYT, KETLUAN) values ('HS0003', 'BN0004', '2020-01-02', N'Soi buồng tử cung với dung dịch Sorbitol', 'NV0018', 'CK0003', 'CS0003', N'Rối loạn xuất tinh');
+INSERT INTO HSBA (MAHSBA, MABN, NGAY, CHANDOAN, MABS, MAKHOA, MACSYT, KETLUAN) values ('HS0004', 'BN0006', '2020-01-02', N'Nhuộm hóa mô miễn dịch cho mỗi một dấu ấn', 'NV0019', 'CK0004', 'CS0004', N'Sa sinh dục');
+INSERT INTO HSBA (MAHSBA, MABN, NGAY, CHANDOAN, MABS, MAKHOA, MACSYT, KETLUAN) values ('HS0005', 'BN0007', '2020-01-02', N'Chọc dò tủy sống', 'NV0020', 'CK0005', 'CS0005', N'Đau nửa đầu');
+
+--INSERT DATA HSBA_DV
+INSERT INTO HSBA_DV (MAHSBA, MADV, NGAY, MAKTV, KETQUA) values ('HS0001', 'DV0001', '2020-01-01', 'NV0016', N'Chấn thương dây chằng chéo trước');
+INSERT INTO HSBA_DV (MAHSBA, MADV, NGAY, MAKTV, KETQUA) values ('HS0002', 'DV0002', '2020-01-02', 'NV0017', N'Sa trực tràng');
+INSERT INTO HSBA_DV (MAHSBA, MADV, NGAY, MAKTV, KETQUA) values ('HS0003', 'DV0003', '2020-01-02', 'NV0018', N'Rối loạn xuất tinh');
+INSERT INTO HSBA_DV (MAHSBA, MADV, NGAY, MAKTV, KETQUA) values ('HS0004', 'DV0004', '2020-01-02', 'NV0019', N'Sa sinh dục');
+INSERT INTO HSBA_DV (MAHSBA, MADV, NGAY, MAKTV, KETQUA) values ('HS0005', 'DV0005', '2020-01-02', 'NV0020', N'Đau nửa đầu');
